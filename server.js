a73327df2296a4aa372b7ded9a3dfe36bcfe5339
Alfredo Del Fabro Neto <alfredodfn@gmail.com>
2024-06-30 12:27:20 -0300

    add request handlers to improve code quality

--- server.js
+++ server.js
@@ -20,28 +20,44 @@ const jsonMiddkeware = (req, res, next) => {
   next();
 };
 
+// Route handler for GET /api/users
+const getUsersHandler = (req, res) => {
+  res.write(JSON.stringify(users));
+  res.end();
+};
+
+// Route handler for GET /api/users/:id
+const getUserByIdHandler = (req, res) => {
+  const id = req.url.split('/')[3];
+  const user = users.find((user) => user.id === parseInt(id));
+  if (user) {
+    res.write(JSON.stringify(user));
+  } else {
+    res.statusCode = 404;
+    res.write(JSON.stringify({ message: 'User not found' }));
+  }
+  res.end();
+};
+
+// Not found handler
+const notFoundHandler = (req, res) => {
+  res.statusCode = 404;
+  res.write(JSON.stringify({ message: 'Route not found' }));
+  res.end();
+};
+
 const server = createServer((req, res) => {
   logger(req, res, () => {
     jsonMiddkeware(req, res, () => {
       if (req.url === '/api/users' && req.method === 'GET') {
-        res.write(JSON.stringify(users));
+        getUsersHandler(req, res);
       } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
-        const id = req.url.split('/')[3];
-        const user = users.find((user) => user.id === parseInt(id));
-        if (user) {
-          res.write(JSON.stringify(user));
-        } else {
-          res.statusCode = 404;
-          res.write(JSON.stringify({ message: 'User not found' }));
-        }
+        getUserByIdHandler(req, res);
       } else {
-        res.statusCode = 404;
-        res.write(JSON.stringify({ message: 'Route not found' }));
+        notFoundHandler(req, res);
       }
     });
   });
-  
-  res.end();
 });
 
 server.listen(PORT, () => {
